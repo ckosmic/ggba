cb169ff68a72c4d0369ef2aaac1c29219cfb88df
Christian Kosman <christiankosman@gmail.com>
2017-03-19 18:49:30 -0700

    Update Emulator.js

--- IodineGBA/core/Emulator.js
+++ IodineGBA/core/Emulator.js
@@ -45,7 +45,7 @@ function GameBoyAdvanceEmulator() {
     this.audioNumSamplesTotal = 0;            //Buffer size.
     this.calculateTimings();                  //Calculate some multipliers against the core emulator timer.
     this.generateCoreExposed();               //Generate a limit API for the core to call this shell object.
- print(settings.timerIntervalRate);
+ print(this.settings.timerIntervalRate);
 }
 GameBoyAdvanceEmulator.prototype.generateCoreExposed = function () {
     var parentObj = this;
