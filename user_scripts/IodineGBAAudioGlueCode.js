9ad280cc80fd15c35634faec050adce2d31fc88e
Christian Kosman <christiankosman@gmail.com>
2017-03-20 18:14:34 -0700

    Update IodineGBAAudioGlueCode.js

--- user_scripts/IodineGBAAudioGlueCode.js
+++ user_scripts/IodineGBAAudioGlueCode.js
@@ -102,6 +102,9 @@ GlueCodeMixerInput.prototype.initialize = function (channelCount, sampleRate, bu
 GlueCodeMixerInput.prototype.register = function (volume) {
     this.mixer.appendInput(this);
 }
+function ChangeVolume(volume) {
+    this.changeVolume(volume);
+}
 GlueCodeMixerInput.prototype.changeVolume = function (volume) {
     this.volume = volume;
 }
