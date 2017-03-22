f468528ec80d1618222516528ba6d838f32c0d18
Christian Kosman <christiankosman@gmail.com>
2017-03-21 20:04:22 -0700

    Update Emulator.js

--- IodineGBA/core/Emulator.js
+++ IodineGBA/core/Emulator.js
@@ -15,7 +15,7 @@ function GameBoyAdvanceEmulator() {
         "audioBufferUnderrunLimit":8,       //Audio buffer minimum span amount over x interpreter iterations.
         "audioBufferDynamicLimit":2,        //Audio buffer dynamic minimum span amount over x interpreter iterations.
         "audioBufferSize":20,               //Audio buffer maximum span amount over x interpreter iterations.
-        "timerIntervalRate":128,             //How often the emulator core is called into (in milliseconds).
+        "timerIntervalRate":512,             //How often the emulator core is called into (in milliseconds).
         "emulatorSpeed":2,                  //Speed multiplier of the emulator.
         "metricCollectionMinimum":30,       //How many cycles to collect before determining speed.
         "dynamicSpeed":false                 //Whether to actively change the target speed for best user experience.
